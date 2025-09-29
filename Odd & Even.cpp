bc0ce2b2d91ad8d1e7f79c759cefc12df27b5913
Harshika Kamwal <harshikakamwal@gmail.com>
2025-09-29 18:59:38 +0530

    Odd & Even.cpp

--- Odd & Even.cpp
+++ Odd & Even.cpp
@@ -1,5 +1,5 @@
-// Aditya Jadhav
-// 24070123005
+// Harshika Kamwal
+// 25070123508
 
 #include<iostream>
 using namespace std;
@@ -18,4 +18,5 @@ int main(){
 // OUTPUT
 // Enter the number
 // 11
+
 // The number 11 is ODD
